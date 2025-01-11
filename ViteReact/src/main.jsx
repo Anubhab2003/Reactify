e95fa331b9a9fd1d1f4e718283a32557982b717c
Anubhab2003 <anubhabchowdhury01@gmail.com>
2025-01-11 15:51:37 +0530

    customs

--- ViteReact/src/main.jsx
+++ ViteReact/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>I wanna be Rich</h1>
+      <h1>BatMan</h1>
+    </div>
+  )
+}
+const anotherElement=(
+  <a href="https://youtube.com" target='_blank'>Youtube pe ja</a>
+)
+const user="IronMan"
+const reactElement=React.createElement(
+  'a',
+  {href:'https://youtube.com',target:'_blank'},
+  'Sale Youtube pe chala ja',
+  user
+)
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
+    {/* <MyApp /> */}
+   {/* {anotherElement} */}
+   {/* {reactElement} */}
   </StrictMode>,
 )
