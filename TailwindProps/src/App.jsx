dff09ad96a119c9bc234112d99f5361a98b50a8a
Anubhab2003 <anubhabchowdhury01@gmail.com>
2025-01-13 00:20:04 +0530

    task done

--- TailwindProps/src/App.jsx
+++ TailwindProps/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-green-600">Hello world!</h1>
+      <Card title="WonderLand" description="WonderLand is a Fucking awesome place where we can chill" />
     </>
   );
 }
